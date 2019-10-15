0e7ac90fc74dc0c1367c4d1a0c0ab9db4016b9e5
Ken-Mens <kenneth.mensah@gmail.com>
2019-10-15 20:35:47 +0000

    added functionalities to main which including timing and background

--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,40 @@
+import Compositor from './Compositor.js';
+import Timer from './timer.js';
+import Entity from './Entity.js';
 import {loadLevel} from './loaders.js'; 
-import {loadMainSprite, loadBackgroundSprites} from './sprites.js'; 
-
-function drawBackground(background, context, sprites) {
-	background.ranges.forEach(([x1, x2, y1, y2]) => {
-          for (let x = x1; x < x2; x++) {
-              for(let y = y1; y < y2; y++) {
-                sprites.drawTile(background.tile, context, x, y);
-         }
-       }
-  });
-}
+import {createP} from './entities.js'; 
+import {loadBackgroundSprites} from './sprites.js'; 
+import {createBackgroundLayer, createSpriteLayer} from './layers.js'; 
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
+
+
 Promise.all([
-    loadMainSprite(),
+    createP(),
     loadBackgroundSprites(),
     loadLevel('1-1'),
 ])
-.then(([mainsprite, sprites,level]) => {
+.then(([main_p, backgroundSprites, level]) => {
+   const comp = new Compositor();
+
+   const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+   comp.layers.push(backgroundLayer);
+
+   const gravity = 30;
+    main_p.post.set(64, 180);
+    main_p.vel.set(200, -600);
 
 
-   const post = {
-	x: 64,
-	y: 64,
-};
-	function update() {
-        level.backgrounds.forEach(background => {
-         drawBackground(background, context, sprites);
-    });
-    mainsprite.draw('idle', context, post.x, post.y);
-    post.x += 2;
-    post.y += 2;
-    requestAnimationFrame(update);
-  }
-   update();
+   const spriteLayer = createSpriteLayer(main_p);
+   comp.layers.push(spriteLayer);
+
+   const timer = new Timer(1/60);
+   timer.update = function update(deltaTime) {
+           comp.draw(context);
+           main_p.update(deltaTime);
+           main_p.vel.y += gravity;
+	}
+   timer.start();
 });
