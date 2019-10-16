6f95d07406e5264863bb749fa15435c04be11095
Ken-Mens <kenneth.mensah@gmail.com>
2019-10-16 05:02:16 +0000

    changed functionalities to main script

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,55 @@
 import Compositor from './Compositor.js';
 import Timer from './timer.js';
-import Entity from './Entity.js';
-import {loadLevel} from './loaders.js'; 
-import {createP} from './entities.js'; 
-import {loadBackgroundSprites} from './sprites.js'; 
-import {createBackgroundLayer, createSpriteLayer} from './layers.js'; 
+import {loadLevel} from './loaders.js';
+import {createD} from './entities.js';
+import {loadBackgroundSprites} from './sprites.js';
+import {createBackgroundLayer, createSpriteLayer} from './layers.js';
+import Keyboard from './KeyboardState.js';
 
+//returns element object for game screen
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 
-
 Promise.all([
-    createP(),
+    createD(),
     loadBackgroundSprites(),
     loadLevel('1-1'),
 ])
-.then(([main_p, backgroundSprites, level]) => {
+.then(([dragon, backgroundSprites, level]) => {
    const comp = new Compositor();
 
+// declare and push background layer of game
    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
    comp.layers.push(backgroundLayer);
 
-   const gravity = 30;
-    main_p.post.set(64, 180);
-    main_p.vel.set(200, -600);
+   const gravity = 1900;
+    dragon.post.set(64, 180);
+    dragon.vel.set(200, -600);
 
 
-   const spriteLayer = createSpriteLayer(main_p);
+   //Add mapping for keyboard input and print state
+  const SPACE = 32;
+  const input = new Keyboard(); 
+  input.addMapping(SPACE, keyState => {
+       if (keyState) {
+	 dragon.jump.start();
+	} else {
+		dragon.jump.cancel();
+	}
+        console.log(KeyState);
+  });
+  input.listenTo(window);
+
+
+   const spriteLayer = createSpriteLayer(dragon);
    comp.layers.push(spriteLayer);
 
    const timer = new Timer(1/60);
    timer.update = function update(deltaTime) {
+           dragon.update(deltaTime);
            comp.draw(context);
-           main_p.update(deltaTime);
-           main_p.vel.y += gravity;
+           dragon.vel.y += gravity * deltaTime;
 	}
    timer.start();
 });
