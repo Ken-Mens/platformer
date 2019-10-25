c634ddf8c9395a190a1c5a27a10d03f95d42fc7c
Ken-Mens <kenneth.mensah@gmail.com>
2019-10-25 01:33:05 +0000

    fixed camera script

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,7 +1,10 @@
+// Create a new Camera
+
 import {Vector} from './math.js';
 export default class Camera {
 	constructor() {
 		this.post = new Vector (0, 0);
+		this.size = new Vector (256, 225);
 	}
 }
 
