495955dee120a90b396eff7e157dc57883dfaa5e
Ken-Mens <kenneth.mensah@gmail.com>
2019-10-29 20:38:27 +0000

    add accelartion and deceleration

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -5,9 +5,27 @@ export default class Go extends Trait {
 
          super('go');
 	this.dir = 0;
-	this.speed = 5000;
+	this.accel = 400;
+	this.decel = 300;
+	this.dragging = 1/5000;
+
+	this.distance = 0;
+	this.header = 1;
      }
         update(entity, deltaTime) {
-	entity.vel.x = this.speed * this.dir * deltaTime;
+		const aba = Math.abs(entity.vel.x);
+		entity.vel.x += this.accel * this.dir * deltaTime;
+		this.header = this.dir;
+
+	if (this.dir) {
+		this.distance += aba * deltaTime;
+	}    else if (entity.vel.x !== 0) {
+		const decelx = Math.min(aba, this.decel * deltaTime);
+		entity.vel.x += entity.vel.x > 0 ? -decelx : decelx;
+	} else {
+		this.distance = 0;
 	   }
+		const drag = this.dragging * entity.vel.x * Math.abs(entity.vel.x);
+		entity.vel.x -= drag;
         }
+}
